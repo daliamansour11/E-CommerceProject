05402f2085d96e5fa8e0cda3b64976ce5b45232e
ShroukMamdouhAbdElAziz <104698688+ShroukMamdouhAbdElAziz@users.noreply.github.com>
2022-05-31 18:51:14 +0200

    Home Design

---
+++ app/src/main/java/com/example/e_commerceproject/home/model/BrandModel.kt
@@ -0,0 +1,7 @@
+package com.example.e_commerceproject.home.model
+
+
+data class BrandModel(
+    var brandname:String,
+    var brandImage:Int
+)
--- app/src/main/java/com/example/e_commerceproject/home/view/HomeFragment.kt
+++ app/src/main/java/com/example/e_commerceproject/home/view/HomeFragment.kt
@@ -5,56 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.e_commerceproject.R
+import com.example.e_commerceproject.home.model.DummyData
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var brandsAdapter: BrandsAdapter
+    lateinit var homeFragmentView: View
+    lateinit var brandLogo: ImageView
+    lateinit var profileScreen: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+
+    }
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        homeFragmentView = inflater.inflate(R.layout.fragment_home, container, false)
+        return homeFragmentView
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView=view.findViewById(R.id.brandsRecycleView)
+        val brandsLinearLayoutManager: LinearLayoutManager = LinearLayoutManager(context)
+        brandsLinearLayoutManager.orientation= RecyclerView.HORIZONTAL
+        brandsAdapter= BrandsAdapter(requireContext())
+        brandsAdapter.setDataList(DummyData.BRAND_DATA)
+        recyclerView.layoutManager=brandsLinearLayoutManager
+        recyclerView.adapter=brandsAdapter
+
+        profileScreen=homeFragmentView.findViewById(R.id.profileIconScreen)
+
+        profileScreen.setOnClickListener(View.OnClickListener {
+            val fragmentManager=parentFragmentManager
+            val fragmentTransaction=fragmentManager.beginTransaction()
+
+//            fragmentTransaction.replace(R.id.,fragment)
+            fragmentTransaction.commit()
+        })
+
     }
+
+
 }
