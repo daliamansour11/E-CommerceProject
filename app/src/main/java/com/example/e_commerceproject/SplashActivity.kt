57adccf1206d3a0a94d4c840c7cc8ab7212d2b2c
daliamansour11 <daliamansour297@gmail.com>
2022-06-18 00:33:04 +0200

    splash

---
+++ app/src/main/java/com/example/e_commerceproject/SplashActivity.kt
@@ -0,0 +1,62 @@
+package com.example.e_commerceproject
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var splash_img: ImageView
+    val APLASH_SCREEN =5000
+    private lateinit var topAnimation :Animation
+    private lateinit var bottomAnimation :Animation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+ topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation)
+ bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation)
+        splash_img = findViewById(R.id.splash)
+
+       splash_img.animation = bottomAnimation
+
+        Handler().postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 3000)
+
+
+//        makeSplashFullScreen()
+//        startAnimation()
+       // splashTimer()
+    }
+
+//    private fun makeSplashFullScreen() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            window.statusBarColor = Color.WHITE
+//        }
+//    }
+//
+//    private fun startAnimation() { // alpha animation
+//        val alphaAnimation = AlphaAnimation(0.0f, 0.0f)
+//        alphaAnimation.duration = 2000
+//        splash_img = findViewById(R.id.splash)
+//        splash_img.startAnimation(alphaAnimation)
+//
+//    }
+//
+//    private fun splashTimer() {
+//
+//    }
+}
