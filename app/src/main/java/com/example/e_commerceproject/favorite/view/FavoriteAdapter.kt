d87b9d37696d69da842971c0cfe4f145e259fcd2
Narges Nagy <nargesnagy21@gmail.com>
2022-06-23 16:41:25 +0200

    dev favorite

---
+++ app/src/main/java/com/example/e_commerceproject/favorite/view/FavoriteAdapter.kt
@@ -0,0 +1,76 @@
+package com.example.e_commerceproject.favorite.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.e_commerceproject.R
+import com.example.e_commerceproject.cart.model.DraftOrder
+import com.example.e_commerceproject.cart.model.LineItem
+
+class FavoriteAdapter (var context: Context , val onDeletefromFavoriteClikListener: OnDeletefromFavoriteClikListener) : RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
+
+    var data: List<DraftOrder> = ArrayList()
+    var lineItems: List<LineItem> = ArrayList()
+
+    fun setlist(dataList: List<DraftOrder>){
+        this.data = dataList
+    }
+    fun setDatalist(dataList: List<LineItem>){
+        this.lineItems = dataList
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var image: ImageView
+        var name: TextView
+        var price : TextView
+        var deletefromFav : Button
+        var addtoCart : Button
+
+
+        init {
+            image = itemView.findViewById(R.id.favorite_imageView)
+            name = itemView.findViewById(R.id.fav_name_textview)
+            price = itemView.findViewById(R.id.favorite_price_textview)
+            deletefromFav = itemView.findViewById(R.id.favorite_delete_from_favorieButton)
+            addtoCart = itemView.findViewById(R.id.favorite_addtocartbtn)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteAdapter.ViewHolder {
+
+        // Inflate the custom layout
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.favoriecustomrow, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FavoriteAdapter.ViewHolder, position: Int) {
+
+        var data = data[position]
+
+
+        if(data.line_items?.count() != 0 ){
+            holder.name.text = ("${data.line_items?.get(0)?.title}")
+            holder.price.text = ("${data.line_items?.get(0)?.price}$")
+
+        }
+
+        if(data.note_attributes?.count() != 0 ){
+            Glide.with(context).load(data.note_attributes?.get(0)?.value)
+                .placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+                .into(holder.image)
+        }
+        holder.deletefromFav.setOnClickListener {
+            onDeletefromFavoriteClikListener.onDeleteFromFavClicked("${data.id}")
+        }
+
+    }
+
+    override fun getItemCount() = data.size
+}
--- app/src/main/java/com/example/e_commerceproject/profile/view/ProfileFragment.kt
+++ app/src/main/java/com/example/e_commerceproject/profile/view/ProfileFragment.kt
@@ -13,12 +13,23 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.e_commerceproject.R
 import com.example.e_commerceproject.Settings.view.SettingsFragment
 import com.example.e_commerceproject.authentication.login.view.LoginFragment
 import com.example.e_commerceproject.authentication.register.view.RegisterFragment
+import com.example.e_commerceproject.cart.model.DraftOrder
 import com.example.e_commerceproject.common.network.NetworkUtils
+import com.example.e_commerceproject.favorite.view.FavoriteAdapter
+import com.example.e_commerceproject.favorite.view.FavoriteFragment
+import com.example.e_commerceproject.favorite.view.OnDeletefromFavoriteClikListener
+import com.example.e_commerceproject.favorite.viewmodel.FavoriteViewModel
+import com.example.e_commerceproject.favorite.viewmodel.FavoriteViewModelFactory
 import com.example.e_commerceproject.home.view.HomeFragment
+import com.example.e_commerceproject.network.FavoriteRepository
+import com.example.e_commerceproject.network.remotesource.RetrofitService
 import com.example.e_commerceproject.profile.client.ProfileClient
 import com.example.e_commerceproject.profile.model.OrderModel
 import com.example.e_commerceproject.profile.model.ProfileRepository
@@ -40,6 +51,11 @@ class ProfileFragment : Fragment() {
     lateinit var viewModel: ProfileViewModel
     lateinit var vmFactory: ProfileViewModelFactory
     private var orderList:ArrayList<OrderModel> = ArrayList()
+
+    private lateinit var favoriteAdapter: WishesAdapter
+    lateinit var wishesRecyclerView: RecyclerView;
+    lateinit var favoriteviewModel: FavoriteViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,6 +87,7 @@ class ProfileFragment : Fragment() {
         var profile_frg  = inflater.inflate(R.layout.fragment_profile, container, false)
 //        moreWishes_btn = profile_frg.findViewById(R.id.morewish_btn)
         moreorder_btn = profile_frg.findViewById(R.id.moreOrder_btn)
+        moreWishes_btn = profile_frg.findViewById(R.id.morewish_btn)
         profile_back = profile_frg.findViewById(R.id.profile_back)
         profile_settings = profile_frg.findViewById(R.id.profile_settings)
 
@@ -145,20 +162,42 @@ class ProfileFragment : Fragment() {
 
         })
 
-//        moreWishes_btn.setOnClickListener(object : View.OnClickListener {
-//            override fun onClick(view: View?) {
-//
-//                val moreWishesFragment = MoreWishesFragment()
-//                fragmentManager?.beginTransaction()?.replace(R.id.fragmentContainerView, moreWishesFragment)?.commit()
-//                Toast.makeText(context, "go to morewishes ", Toast.LENGTH_LONG).show()
-//
-//            }
-////                val intent = Intent(this,HomeFragment ::class.java)
-////                intent.putExtra("key", "Kotlin")
-////                startActivity(intent)
-////            }
-//
-//        })
+        // wishes
+        wishesRecyclerView = view.findViewById(R.id.profile_favorite_recycleview)
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = RecyclerView.VERTICAL
+        wishesRecyclerView.layoutManager = layoutManager
+        //wishesRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        favoriteAdapter = WishesAdapter(requireContext())
+        wishesRecyclerView.adapter = favoriteAdapter
+
+        val retrofitService = RetrofitService.getInstance()
+        val mainRepository = FavoriteRepository(retrofitService)
+        favoriteviewModel = ViewModelProvider(this, FavoriteViewModelFactory(mainRepository)).get(FavoriteViewModel::class.java)
+        favoriteviewModel.getFavoriteProducts()
+
+        favoriteviewModel.favoriteProducts.observe(viewLifecycleOwner, {
+
+            var ar = ArrayList<DraftOrder>()
+            ar.add(it.draft_orders[0])
+            ar.add(it.draft_orders[1])
+            favoriteAdapter.setlist(ar)
+            favoriteAdapter.notifyDataSetChanged()
+            it.draft_orders.filter { "email" == "jkjkjk@gmail.com" && "note" == "fav" }
+            Log.i("TAG", "onViewCreatedjjjjjjjjjjjjjjjjjjjjjjjjjj: ${it.draft_orders.filter { "email" == "reham33@gmail.com" }}")
+            // Log.i("TAG", "onViewCreatedjjjjjjjjjjjjjjjjjjjjjjjjjj: ${it.draft_orders }")
+
+
+        })
+
+
+        moreWishes_btn.setOnClickListener{
+
+                val moreWishesFragment = FavoriteFragment()
+                fragmentManager?.beginTransaction()?.replace(R.id.fragmentContainerView, moreWishesFragment)?.commit()
+
+            }
+
         login_btn.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View?) {
 
@@ -216,6 +255,7 @@ fun ChangeButtonText( text : String){
     login_btn.setText(text)
 
 }
+
 
 }
 /*override fun onMoreOrderClicked() {
